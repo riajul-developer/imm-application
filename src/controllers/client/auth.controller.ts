39ef9bda25cd5f701be1a4ba5a57d48db47ae98d
raihandevtech <raihan.dev3@gmail.com>
2025-06-27 17:51:06 +0600

    add profile data in verify otp

--- src/controllers/client/auth.controller.ts
+++ src/controllers/client/auth.controller.ts
@@ -3,6 +3,9 @@ import { ZodError } from 'zod'
 import * as authService from '../../services/client/auth.service'
 import { sendOtpSchema, verifyOtpSchema } from '../../schemas/otp.schema'
 import { badErrorResponse, serverErrorResponse, successResponse } from '../../utils/response.util'
+import { getUserProfile } from '../../services/client/profile.service'
+import fastifyJwt from '@fastify/jwt'
+import { jwt } from 'twilio'
 
 export const sendOtp = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
@@ -24,7 +27,13 @@ export const verifyOtp = async (request: FastifyRequest, reply: FastifyReply) =>
   try {
     const { phoneNumber, otp } = verifyOtpSchema.parse(request.body)
     const token = await authService.verifyOtp(phoneNumber, otp, request)
-    return successResponse(reply, 'OTP verified successfully', { token })
+
+    const decoded = request.server.jwt.verify(token) as any
+    const userId = decoded.userId
+
+    const profile = await getUserProfile(userId);
+
+    return successResponse(reply, 'OTP verified successfully', { token, profile })
   } catch (error) {
     if (error instanceof ZodError) {
       return badErrorResponse(reply, 'Validation failed', error.errors.map(e => ({
