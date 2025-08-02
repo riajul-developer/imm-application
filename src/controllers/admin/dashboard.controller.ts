bb25255417180bfc4c5e5a99ba4b6588957ac70c
raihandevtech <raihan.dev3@gmail.com>
2025-08-02 23:31:17 +0600

    implement dashboard page filter

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,14 +1,33 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import * as dashboardService from '../../services/dashboard.service'
-import { serverErrorResponse, successResponse } from '../../utils/response.util'
+import { badErrorResponse, serverErrorResponse, successResponse } from '../../utils/response.util'
 
 export const getDashboard = async (request: FastifyRequest, reply: FastifyReply) => {
   try {
-    const dashboard = await dashboardService.dashboard()
+    const { startDate, endDate } = request.query as { 
+      startDate?: string; 
+      endDate?: string; 
+    };
+    
+    if (startDate && !isValidDate(startDate)) {
+      return badErrorResponse(reply, 'Invalid startDate format. Use YYYY-MM-DD')
+    }
+    
+    if (endDate && !isValidDate(endDate)) {
+      return badErrorResponse(reply, 'Invalid endDate format. Use YYYY-MM-DD')
+    }
+    
+    const dashboard = await dashboardService.dashboard(startDate, endDate);
+
     return successResponse(reply, 'Dashboard data retrieved successfully.', dashboard)
   } catch (error) {
     return serverErrorResponse(reply, 'Something went wrong.')
   }
+}
+
+function isValidDate(dateString: string): boolean {
+  const date = new Date(dateString);
+  return date instanceof Date && !isNaN(date.getTime()) && !!dateString.match(/^\d{4}-\d{2}-\d{2}$/);
 }
 
 export const getRecentApplications = async (request: FastifyRequest, reply: FastifyReply) => {
